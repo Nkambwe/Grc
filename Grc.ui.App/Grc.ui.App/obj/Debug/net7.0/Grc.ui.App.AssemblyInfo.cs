539860e1b118e8c346de86b8e226dfd5266f2868
Nkambwe <jo.jomac.mac801@gmail.com>
2025-07-14 03:34:25 +0300

    Correct password validation

--- Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
+++ Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8838627223ff5731b912b98ea7fe7733c97e1d23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03d5f899a6a7c500edab4f5b14c7091600a15f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
