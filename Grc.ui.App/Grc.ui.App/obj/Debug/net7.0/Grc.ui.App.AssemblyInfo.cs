7ad638ada3321cd6f6579c82bc75f623979bbd6a
Nkambwe <jo.jomac.mac801@gmail.com>
2025-07-28 05:43:28 +0300

    Authenticating user_01

--- Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
+++ Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4162fc618b2127d196f03869cc45dc4190e9ae69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203caad7ca07926d5ee4f13206f141ed32d0c08f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
