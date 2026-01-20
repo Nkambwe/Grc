d06c46746f10b8d485a7e03e3a4d6fe87261a760
Nkambwe <jo.jomac.mac801@gmail.com>
2026-01-20 05:11:25 +0300

    Align System tables

--- Grc.Middleware.Api/Grc.Middleware.Api/obj/Debug/net7.0/Grc.Middleware.Api.AssemblyInfo.cs
+++ Grc.Middleware.Api/Grc.Middleware.Api/obj/Debug/net7.0/Grc.Middleware.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grc.Middleware.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed1dba8c7f0a768770aa5b55ebd775acb48a820f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0f0dffabd746ed676f2ae7721ecff422667b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grc.Middleware.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grc.Middleware.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
