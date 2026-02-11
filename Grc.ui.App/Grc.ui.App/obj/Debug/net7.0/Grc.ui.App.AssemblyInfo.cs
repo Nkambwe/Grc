baced493d36e6869ea1b814b5312b68184c26ce3
John Mark Nkambwe <mark.nkambwe@postbank.co.ug>
2026-02-11 09:15:24 +0300

    Clean up

--- Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
+++ Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd21e77ab77c292294024cfa89eb422bee9a5e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0469b34c4674c2576addcc8a32799f53a19374cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
