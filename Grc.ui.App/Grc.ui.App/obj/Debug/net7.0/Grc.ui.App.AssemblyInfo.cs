1cd6d208cd2cf2b0f113373b4b3b0b0d29035b8e
John Mark Nkambwe <mark.nkambwe@postbank.co.ug>
2026-02-12 18:32:33 +0300

    Add settings

--- Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
+++ Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b511d75743a09aee29b3c0f9b92f15187cd5c774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f68aacd14027e2fb84a7f6e4bfd9f1c12cac1a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
