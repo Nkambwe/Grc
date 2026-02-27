111cf6e8dfbed15b5bcb86bf70d6ba90f07b881a
John Mark Nkambwe <mark.nkambwe@postbank.co.ug>
2026-02-27 07:28:00 +0300

    Some dlls_11

--- Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
+++ Grc.ui.App/Grc.ui.App/obj/Debug/net7.0/Grc.ui.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f0c78d4790a6168de92337c37156950d461fe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1b655e56bf5100b197f90d2dfba5cc4b947ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grc.ui.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
