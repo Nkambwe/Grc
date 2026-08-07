58257dda5d1ff95edb68564af5869aba1247cb4b
John Mark Nkambwe <mark.nkambwe@postbank.co.ug>
2026-08-07 10:09:37 +0300

    Correct obligation data

--- Grc.Middleware.Api/Grc.Middleware.Api/obj/Debug/net7.0/Grc.Middleware.Api.AssemblyInfo.cs
+++ Grc.Middleware.Api/Grc.Middleware.Api/obj/Debug/net7.0/Grc.Middleware.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grc.Middleware.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd16adb98ee7e6a3d337ca70736e11053194ae57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdde82ca98e35bcf3590aefbc6f358fb2dec79fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grc.Middleware.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grc.Middleware.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
